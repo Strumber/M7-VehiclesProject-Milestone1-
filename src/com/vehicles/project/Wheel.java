254769f38d6aaa8c0df1a0ad879bb89b030f78bb
Strumber <mzgjar@protonmail.com>
2021-04-22 18:50:50 +0200

    classe Wheel modificada Milestone 1 Fase 2

--- src/com/vehicles/project/Wheel.java
+++ src/com/vehicles/project/Wheel.java
@@ -7,7 +7,7 @@ public class Wheel {
 	public Wheel(String brand, double diameter)throws Exception { //constructor
 		
 		this.brand = brand;
-		if (this.diameter>0.4 || this.diameter <4.0) throw new Exception ("el diametre ha de ser superior a 0.4 i inferior a 4");
+		if (diameter<0.4 || diameter >4) throw new Exception ("el diametre ha de ser superior a 0.4 i inferior a 4");
 			//this.diameter = diameter;
 		
 			System.out.println();
